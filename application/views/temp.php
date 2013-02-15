a32ff010d64bf1a62038ef489e1165ea0ccc3cf8
R. Groot <rein.groot@gmail.com>
2013-02-15 11:22:48 +0100

    Setup of temp site shwoing just my business card

--- application/views/temp.php
+++ application/views/temp.php
@@ -1,3 +1,6 @@
-<div class="content rg-temp">
-	This is the temp image
-</div>
+<section class="content rg-temp">
+	<a href="mailto:welcome@reingroot.nl">
+		<img src="img/reingroot_back_medium.png" alt="Rein Groot - Strategy, Development & Delivery" class="rg-business-card-back" />
+		<img src="img/reingroot_front_medium.png" alt="Rein Groot - Strategy, Development & Delivery" class="rg-business-card-front" />
+	</a>
+</section>
