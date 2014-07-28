43a70f8204ba99c5b2534019ad46d433839b7523
R. Groot <welcome@reingroot.nl>
2014-07-28 21:56:16 +0200

    Got the r.js uglify and optimizer running. Next up is to implement versioning with the build.

--- gruntfile.js
+++ gruntfile.js
@@ -26,59 +26,70 @@ module.exports = function (grunt) {
                 "reporter" : "xunit",
                 "output" : "test/target/mocha/results/result.xml"
             }
-        }
+        },
 
-//        requirejs : {
-//            compile : {
-//                options : {
-//                    appDir  : "js",
-//                    baseUrl : "../",
-//                    dir     : "test/target/dest",
-//                    paths   : {
-//                        "mods" : "../js/modules",
-//                        "vendor" : "../js/vendor"
-//                    },
-//                    optimize : "uglify",
-//                    uglify : {
-//                        ascii_only : true
-//                    },
-//                    preserveLicenseComments: true,
-//                    findNestedDependencies: true,
-//                    optimizeCss: "standard.keepLines",
-//                    preserveLicenceComments : true,
-//                    modules : [
-//                        {
-//                            name :
-//                                'mods/active-link-higlighter',
-//                            exclude : [
-//                                'vendor/jquery.ba-bbq.js',
-//                                'vendor/jquery.ba-bbq.min.js',
-//                                'vendor/modernizr-2.6.2.min.js',
-//                                'jquery.js',
-//                                'require.js'
-//                            ]
-//                        },
-//                        {
-//                            name :
-//                                'mods/item-loader',
-//                            exclude : [
-//                                'vendor/jquery.ba-bbq.js',
-//                                'vendor/jquery.ba-bbq.min.js',
-//                                'vendor/modernizr-2.6.2.min.js',
-//                                'jquery.js',
-//                                'require.js'
-//                            ]
-//                        }
-//                    ]
-//                }
-//            }
-//        }
+        requirejs : {
+            compile : {
+                options : {
+                    appDir  : "./js",
+                    baseUrl : "./",
+                    dir     : "test/target/build",
+                    paths   : {
+                        "mods" : "modules",
+                        "vendor" : "vendor"
+                    },
+                    optimize : "uglify2",
+                    uglify2 : {
+                        output: {
+                            beautify: false
+                        },
+                        compress: {
+                            sequences: false,
+                            global_defs: {
+                                DEBUG: false
+                            }
+                        },
+                        warnings: true,
+                        mangle: true
+                    },
+                    preserveLicenseComments: true,
+                    findNestedDependencies: true,
+                    optimizeCss: "standard.keepLines",
+                    preserveLicenceComments : true,
+                    modules : [{
+                        name :
+                            'mods/active-link-highlighter',
+                        exclude : [
+                            'vendor/jquery.ba-bbq',
+                            'jquery'
+                        ]
+                    },
+                    {
+                        name :
+                            'mods/item-loader',
+                        exclude : [
+                            'vendor/jquery.ba-bbq',
+                            'jquery'
+                        ]
+                    },
+                    {
+                        name :
+                            'mods/fixed-main-nav',
+                        exclude : [
+                            'vendor/viewport',
+                            'jquery'
+                        ]
+                    }]
+                }
+            }
+        }
     });
 
     // load tasks
 //    grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-phantomjs');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
 
     // Install the git hooks
     grunt.registerTask('install_hooks', function () {
